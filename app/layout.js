a6f2bf8180bb465508ac3eb29f0991b1f034ee7f
szelingtan <138017285+szelingtan@users.noreply.github.com>
2025-03-09 15:24:03 +0800

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <title>NUSC Valentines Day</title>
+        <title>Timmy Adventures</title>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
